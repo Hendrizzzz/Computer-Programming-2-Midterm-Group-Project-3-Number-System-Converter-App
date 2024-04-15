57c0e1ea4639b57a70d44169c3c9aed8d8c1a855
Yu <2200277@slu.edu.ph>
2024-04-15 23:54:55 +0800

    EquivalentNumbers.java add binaryToDecimal and isValidBinaryString contents + documentation NumberConverter.java - removed unnecessary methods

--- src/prog2/samcis/EquivalentNumbers.java
+++ src/prog2/samcis/EquivalentNumbers.java
@@ -18,8 +18,11 @@ public class EquivalentNumbers implements NumberConverter{
      * @author...
      * @param binaryString
      */
-    public void setBinaryString(String binaryString) {
+    public void setBinaryString(String binaryString) throws Exception {
         this.binaryString = binaryString;
+        decimal = binaryToDecimal(binaryString);
+        //octalString = Integer.toOctalString(decimal);
+        //hexadecimalString = Integer.toHexString(decimal);
     }
 
 
@@ -68,36 +71,35 @@ public class EquivalentNumbers implements NumberConverter{
     }
 
 
-
-
+    /**
+     * Converts a String representation of a binary number to an integer representation of a decimal number.
+     * The binary string firstly undergoes to a method called isValidBinaryString() to identify if the binary string is valid.
+     * If the binary string is not valid, an Exception will occur.
+     * If the binary string is valid, the binary string will then be extracted character by character to be used in the conversion process.
+     *
+     * @author Oliver Yu
+     * @param binary number in String
+     * @return decimal in Integer type.
+     */
     @Override
-    public String binaryToDecimal(String binary) {
-        return null;
+    public int binaryToDecimal(String binary) throws Exception {
+        int result = 0;
+        if (!isValidBinaryString(binary))
+            throw new Exception("Invalid Binary Number");
+        for (int i = 0; i < binary.length(); i++) {
+            result += (int) (Integer.parseInt("" + binary.charAt(i)) * Math.pow(2, (int) (binary.length() - 1 - i)));
+        }
+        return 0;
     }
 
     @Override
-    public String binaryToOctal(String binary) {
-        return null;
+    public int octalToDecimal(String octal) {
+        return 0;
     }
 
     @Override
-    public String binaryToHexadecimal(String binary) {
-        return null;
-    }
-
-    @Override
-    public String octalToBinary(String octal) {
-        return null;
-    }
-
-    @Override
-    public String octalToDecimal(String octal) {
-        return null;
-    }
-
-    @Override
-    public String octalToHexadecimal(String octal) {
-        return null;
+    public int hexadecimalToDecimal(String hexadecimal) {
+        return 0;
     }
 
     @Override
@@ -115,18 +117,23 @@ public class EquivalentNumbers implements NumberConverter{
         return null;
     }
 
-    @Override
-    public String hexadecimalToBinary(String hexadecimal) {
-        return null;
-    }
-
-    @Override
-    public String hexadecimalToOctal(String hexadecimal) {
-        return null;
-    }
-
-    @Override
-    public String hexadecimalToDecimal(String hexadecimal) {
-        return null;
+    /**
+     * This method is to check the validity of the binary string.
+     * The method uses a for-loop to check each character of the binary string.
+     * If one of the character of the binary string is other than '1' or '2', the method will return false.
+     *
+     * @author Oliver Yu
+     * @param binaryString to be verified.
+     * @return true if the binaryString is a valid binary number. Otherwise, false.
+     */
+    private boolean isValidBinaryString(String binaryString) {
+        boolean result = true;
+        for (int index = 0; index < binaryString.length(); index++) {
+            if (binaryString.charAt(index) != '0' && binaryString.charAt(index) != '1') {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 }
--- src/prog2/samcis/NumberConverter.java
+++ src/prog2/samcis/NumberConverter.java
@@ -1,19 +1,12 @@
 package prog2.samcis;
 
 public interface NumberConverter {
-    String binaryToDecimal(String binary);
-    String binaryToOctal(String binary);
-    String binaryToHexadecimal(String binary);
-
-    String octalToBinary(String octal);
-    String octalToDecimal(String octal);
-    String octalToHexadecimal(String octal);
+    int binaryToDecimal(String binary) throws Exception;
+    int octalToDecimal(String octal);
+    int hexadecimalToDecimal(String hexadecimal);
 
     String decimalToBinary(double decimal);
     String decimalToOctal(double decimal);
     String decimalToHexadecimal(double decimal);
 
-    String hexadecimalToBinary(String hexadecimal);
-    String hexadecimalToOctal(String hexadecimal);
-    String hexadecimalToDecimal(String hexadecimal);
 }
