8c34057e2e2fa69a43219a57c817007b42187ab7
Hendrixx <139997209+Hendrizzzz@users.noreply.github.com>
2024-04-19 21:33:51 +0800

    Update NumberConverter.java

--- src/prog2/samcis/NumberConverter.java
+++ src/prog2/samcis/NumberConverter.java
@@ -2,13 +2,10 @@ package prog2.samcis;
 
 public interface NumberConverter {
     double binaryToDecimal(String binary) throws Exception;
-    int octalToDecimal(String octal);
-    int hexadecimalToDecimal(String hexadecimal);
+    double octalToDecimal(String octal);
+    double hexadecimalToDecimal(String hexadecimal);
 
     String decimalToBinary(double decimal);
     String decimalToOctal(double decimal);
     String decimalToHexadecimal(double decimal);
-
-
-
 }
